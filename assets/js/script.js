8739ddc9f837aef8d885648335441ad62ec818d9
Gustavo Savi <gutosavi@hotmail.com>
2026-03-02 08:03:47 -0300

    style: ajustes no html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 import initApi from "./modules/api-cep.js";
 import validaFormulario from "./modules/validacoes.js";
 
-// const nomeCidade = document.querySelector("#input-cidade");
-
 function initSearch() {
   document.querySelector("#formulario").addEventListener("submit", (e) => {
     e.preventDefault();
